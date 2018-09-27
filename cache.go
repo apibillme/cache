3267cc5c4a8b498cefa7427cbc590954f7ba5e13
Bevan Hunt <bevan@bevanhunt.com>
2018-09-26 18:09:34 -0700

    update godoc

--- cache.go
+++ cache.go
@@ -1,4 +1,4 @@
-// Package ttlru provides a simple, goroutine safe, cache with a fixed number of
+// Package cache provides a simple, goroutine safe, cache with a fixed number of
 // entries. Each entry has a per-cache defined TTL. This TTL is reset on both
 // modification and access to the value. As a result, if the cache is full, and
 // no items have expired, when adding a new item, the item with the soonest
